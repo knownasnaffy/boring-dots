17ed8e999bac7b323d34bd25518b7632fa271e53
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-04-19 16:44:11 +0530

    Format setup.sh taking textwidth into account

--- setup.sh
+++ setup.sh
@@ -1,38 +1,47 @@
 option=$1
 
 if rpm -q stow gum &>/dev/null; then
-  gum log --time kitchen --structured --level debug "Base pkgs are already available."
+  gum log --time kitchen --structured --level debug \
+    "Base pkgs are already available."
 else
   sudo dnf install -y gum stow
-  gum log --time kitchen --structured --level info "Base pkgs successfully installed."
+  gum log --time kitchen --structured --level info \
+    "Base pkgs successfully installed."
 fi
 
 default_packages=(
-    aria2 brightnessctl btop git fastfetch fuse fuse-libs maim python3-xlib qutebrowser ripgrep slop xev zathura zathura-pdf-poppler zsh tealdeer pass
+  aria2 brightnessctl btop git fastfetch fuse fuse-libs maim
+  python3-xlib qutebrowser ripgrep slop xev zathura
+  zathura-pdf-poppler zsh tealdeer pass
 )
 
 optional_packages=(
-    pandoc texlive-schema-full sane-backends simple-scan rustup obs-studio
+  pandoc texlive-schema-full sane-backends simple-scan rustup
+  obs-studio
 )
 
 case "$option" in
   uninstall)
-    gum log --time kitchen --structured --level info "Attempting to remove dotfiles..."
+    gum log --time kitchen --structured --level info \
+      "Attempting to remove dotfiles..."
     stow -v -t "$HOME" --dotfiles -D user-config
     stow -v -t "$HOME/.local/bin" --dotfiles -D bin
     ;;
 
   *)
-    gum log --time kitchen --structured --level info "Attempting to install dotfiles..."
+    gum log --time kitchen --structured --level info \
+      "Attempting to install dotfiles..."
 
     mkdir -p "$HOME/.config" "$HOME/.local/bin"
     stow -v -t "$HOME" --dotfiles user-config
     stow -v -t "$HOME/.local/bin" --dotfiles bin
 
     if rpm -q stow gum idk &>/dev/null; then
-      gum log --time kitchen --structured --level debug "Default pkgs are already available."
+      gum log --time kitchen --structured --level debug \
+        "Default pkgs are already available."
     else
-      gum log --time kitchen --structured --level warn "Missing default packages. Continue to install?"
+      gum log --time kitchen --structured --level warn
+      "Missing default packages. Continue to install?"
 
       option1="Yes (Install default pkgs)"
       option2="No (Don't install anything)"
@@ -41,18 +50,21 @@ case "$option" in
 
       case "$selection" in
         "$option1")
-          gum log --time kitchen --structured --level info "Configuring repositories"
+          gum log --time kitchen --structured --level info \
+            "Configuring repositories"
           ;;
 
         "$option3")
-          gum log --time kitchen --structured --level info "Choose repositories to be installed"
+          gum log --time kitchen --structured --level info \
+            "Choose repositories to be installed"
           ;;
 
         *)
-          gum log --time kitchen --structured --level warn "Skipping package installation. This may result in a weird behaviour if some packages are not present."
+          gum log --time kitchen --structured --level warn \
+            "Skipping package installation. This may result in a weird
+ behaviour if some packages are not present."
           ;;
       esac
     fi
     ;;
 esac
-
