03910bac43e951dcc7366ec740c4f106bc223d05
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-05-24 22:57:40 +0530

    Fix lazydev loading criteria

--- user-config/dot-config/nvim/plugin/40_plugins.lua
+++ user-config/dot-config/nvim/plugin/40_plugins.lua
@@ -192,7 +192,11 @@ now_if_args(function()
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      per_filetype = {
+        -- optionally inherit from the `default` sources
+        lua = { inherit_defaults = true, 'lazydev' }
+      },
       providers = {
         lazydev = {
           name = 'LazyDev',
